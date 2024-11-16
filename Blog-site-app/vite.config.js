f1526208e0e50d4d9bc04a6553ec5f501e6a3a09
Ilya <ilya671qw@gmail.com>
2024-11-16 17:15:37 +0300

    prepare to deploy

--- Blog-site-app/vite.config.js
+++ Blog-site-app/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "react-small-projects/Blog-site-app/"
+  base: "/Blog-site-app/"
 
 })
